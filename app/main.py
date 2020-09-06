486493e75662550c77d6f885dec4864fc254db90
Wilson Freitas <wilson.freitas@gmail.com>
2020-09-06 08:43:54 -0300

    Implemented app to monitor downloads

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+
+import logging
 from datetime import datetime, timedelta, timezone
 
 import pytz
@@ -5,8 +7,12 @@ from flask import Flask
 from flask import render_template
 from flask import request, url_for
 from flask_table import Table, Col
+from flask_table.html import element
+import requests
 
 from google.cloud import datastore
+
+logging.basicConfig(level=logging.INFO)
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -16,6 +22,20 @@ client = datastore.Client()
 SP_TZ = pytz.timezone('America/Sao_Paulo')
 UTC = pytz.utc
 
+
+class URLCol(Col):
+    def __init__(self, name, **kwargs):
+        super(URLCol, self).__init__(name, **kwargs)
+
+    def td_contents(self, item, attr_list):
+        name = self.from_attr_list(item, 'name')
+        refdate = self.from_attr_list(item, 'refdate')
+        # url = '/reprocess?filter={}&refdate={}'.format(name, refdate)
+        url = 'https://us-central1-kyd-storage-001.cloudfunctions.net/kyd-generic-download-publish?filter={}&refdate={}'.format(name, refdate)
+        return element('a', {'href': url}, content='Link')
+
+# -H "Authorization: bearer $(gcloud auth print-identity-token)"
+
 class DownloadLogTable(Table):
     name = Col('Name')
     time = Col('Time')
@@ -24,6 +44,7 @@ class DownloadLogTable(Table):
     status = Col('Status')
     filename = Col('Filename')
     message = Col('Message')
+    url = URLCol('URL')
     allow_sort = True
     classes = ['table', 'table-striped', 'table-sm']
 
@@ -35,18 +56,39 @@ class DownloadLogTable(Table):
         return url_for('index', sort=col_key, direction=direction)
 
 
+@app.route('/reprocess')
+def reprocess():
+    refdate = request.args.get('refdate')
+    name = request.args.get('name')
+    url = 'https://us-central1-kyd-storage-001.cloudfunctions.net/kyd-generic-download-publish?filter={}&refdate={}'.format(name, refdate)
+    res = requests.get(url)
+
+
 @app.route('/')
 def index():
-    date = datetime.now(SP_TZ) + timedelta(-1)
+    key = client.key('Session', 'date')
+    date = client.get(key)
     if request.args.get('date'):
         date = request.args.get('date')
         date = datetime.strptime(date, '%Y-%m-%d')
         date = SP_TZ.localize(date)
+        logging.info('Request date %s', date)
+    elif date:
+        date = date['value']
+        date = date.astimezone(SP_TZ)
+        logging.info('Datastore date %s', date)
+    else:
+        date = datetime.now(SP_TZ) + timedelta(-1)
+        logging.info('Now date %s', date)
+    session_entry = datastore.Entity(key)
+    session_entry.update({'value': date.astimezone(UTC)})
+    client.put(session_entry)
 
     t1 = date.replace(hour=0, minute=0, second=0, microsecond=0)
     t2 = date.replace(hour=23, minute=59, second=59, microsecond=999999)
     t1 = t1.astimezone(UTC)
     t2 = t2.astimezone(UTC)
+    logging.info('%s %s %s', date, t1, t2)
 
     q = client.query(kind='DownloadLog')
     q.add_filter('time', '>=', t1)
@@ -60,14 +102,36 @@ def index():
         log['time'] = log['time'].astimezone(SP_TZ).strftime('%Y-%m-%d %H:%M:%S.%f')
         if log['status'] == 0:
             log['filename'] = 'gs://{}/{}'.format(log['bucket'], log['filename'])
+        else:
+            log['filename'] = ''
+    
+    key = client.key('Session', 'status')
+    status = client.get(key)
+    if request.args.getlist('status'):
+        status = request.args.getlist('status')
+        status = [int(s) for s in status]
+    elif status:
+        status = status['value']
+    else:
+        status = [-1, 0, 1, 2]
+    session_entry = datastore.Entity(key)
+    session_entry.update({'value': status})
+    client.put(session_entry)
 
+    logs = [l for l in logs if l['status'] in status]
     sort = request.args.get('sort', 'name')
     reverse = (request.args.get('direction', 'asc') == 'desc')
     logs_sorted = logs.copy()
     logs_sorted.sort(key=lambda x: x[sort], reverse=reverse)
     tb = DownloadLogTable(logs_sorted, sort_by=sort, sort_reverse=reverse)
 
-    return render_template('base.html', date=date, length=len(logs_sorted), table=tb)
+    return render_template('base.html',
+        date=date,
+        length=len(logs_sorted),
+        status=status,
+        table=tb,
+        sort=sort,
+        direction=request.args.get('direction', 'asc'))
 
 
 if __name__ == '__main__':
