db26c8350bd7b7b0343e14922263dd05146bc57b
wilsonfreitas <wilson.freitas@gmail.com>
2021-04-30 09:48:52 -0300

    Create a separate deploy script for mail functions

---
+++ functions/deploy-mail.ps1
@@ -0,0 +1,4 @@
+
+gcloud functions deploy kyd-sendmail --entry-point gcf_sendmail --runtime python37 --trigger-http --env-vars-file .env.yaml --project=kyd-storage-001
+
+gcloud functions deploy kyd-sendmail --update-env-vars SENDGRID_API_KEY=$env:SENDGRID_API_KEY --project=kyd-storage-001
--- functions/deploy.ps1
+++ functions/deploy.ps1
@@ -4,7 +4,3 @@ gcloud functions deploy kyd-generic-download --entry-point gcf_generic_download 
 gcloud functions deploy kyd-generic-download-publish --entry-point gcf_ignite_generic_downloads --runtime python37 --trigger-http --env-vars-file .env.yaml --project=kyd-storage-001
 
 gcloud functions deploy kyd-save-download-logs --entry-point gcf_save_download_logs --runtime python37 --trigger-topic kyd-storage-download-log --env-vars-file .env.yaml --project=kyd-storage-001
-
-gcloud functions deploy kyd-sendmail --entry-point gcf_sendmail --runtime python37 --trigger-http --env-vars-file .env.yaml --project=kyd-storage-001 --set-env-vars SENDGRID_API_KEY=$env:SENDGRID_API_KEY
-
-gcloud functions deploy kyd-sendmail --update-env-vars SENDGRID_API_KEY=$env:SENDGRID_API_KEY --project=kyd-storage-001 
